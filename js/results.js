2cd0cb51c5f00297c2d60d813bc32dcd7d3c947d
Becky Moore <bmoore@palantir.com>
2016-07-30 13:58:23 -0700

    finishing off the results updates

--- js/results.js
+++ js/results.js
@@ -2,119 +2,286 @@ var app = angular.module('myApp', []);
 
 app.controller('ResultsCtrl', function($scope) {
 
-      //$scope.results = $scope.results2016;
+     $scope.tournaments = Array();
+
+      $scope.getResults = function() {
+          $scope.tournaments = {"2016": Array(),
+                                "2015": Array()};
+          $scope.tournaments["2016"] = $scope.tournaments2016;
+          $scope.tournaments["2015"] = $scope.tournaments2015;
+          return $scope.tournaments;
+      };
+
+      $scope.gamesPlayed = function() {
+          var results = $scope.getResults();
+          var games = 0;
+
+          angular.forEach(results, function(tournaments, season) {
+              angular.forEach(tournaments, function(tournament, key) {
+                  if (tournament.sanctioned){
+                      for (var game in tournament.games){
+                          games++;
+                      }
+                  }
+              });
+            });
+
+          return games;
+      };
+
+      $scope.totalWins = function() {
+          var results = $scope.getResults();
+          var wins = 0;
+
+          angular.forEach(results, function(tournaments, season) {
+              angular.forEach(tournaments, function(tournament, key) {
+                  if (tournament.sanctioned){
+                      angular.forEach (tournament.games, function(game, key) {
+                          if (game.score_us > game.score_them){
+                              wins++;
+                          }
+                      });
+                  }
+              });
+            });
+          return wins;
+      };
+
+      $scope.totalLosses = function() {
+          var results = $scope.getResults();
+          var losses = 0;
+
+          angular.forEach(results, function(tournaments, season) {
+              angular.forEach(tournaments, function(tournament, key) {
+                  if (tournament.sanctioned){
+                      angular.forEach (tournament.games, function(game, key) {
+                          if (game.score_us > game.score_them){
+                          }
+                          else {
+                              losses++
+                          }
+                      });
+                  }
+              });
+            });
+          return losses;
+      };
+
+      $scope.totalPointsScored = function() {
+          var results = $scope.getResults();
+          var points = 0;
+
+          angular.forEach(results, function(tournaments, season) {
+              angular.forEach(tournaments, function(tournament, key) {
+                  if (tournament.sanctioned){
+                      angular.forEach (tournament.games, function(game, key) {
+                          points = points + game.score_us;
+                      });
+                  }
+              });
+            });
+          return points;
+      };
+
+      $scope.totalPointsAgainst = function() {
+          var results = $scope.getResults();
+          var points = 0;
+
+          angular.forEach(results, function(tournaments, season) {
+              angular.forEach(tournaments, function(tournament, key) {
+                  if (tournament.sanctioned){
+                      angular.forEach (tournament.games, function(game, key) {
+                          points = points + game.score_them;
+                      });
+                  }
+              });
+            });
+          return points;
+      };
 
       $scope.tournaments2016 = [
           {
               "tourneyid": 1,
               "sanctioned": false,
               "name": "Beachfront Invitational",
-              "teampic": "img/Beachfront2015_TeamPhoto.JPG",
-              "date": "June 6-7, 2015",
+              "teampic": "img/Beachfront2016_TeamPhoto.JPG",
+              "date": "June 4-5, 2016",
               "location": "Joliet, IL",
               "result": "Not Sanctioned",
-              "infolink": "http://play.usaultimate.org/events/Beachfront-Instant-Classic-Invitational/",
-              "mapslink": "https://goo.gl/maps/uMbDC2MsxYA2",
+              "infolink": "http://www.bfpultimate.com/tournament.html",
+              "mapslink": "https://goo.gl/maps/cXdAiVMp5iz",
               "games": [
                   {
                       "gamenum": 1,
-                      "opponent": "Heist Y",
+                      "opponent": "Sureshot",
                       "type": "Pool Play",
-                      "score_us": 4,
-                      "score_them": 10,
+                      "score_us": 11,
+                      "score_them": 4,
                   },
                   {
                       "gamenum": 2,
-                      "opponent": "Dish",
+                      "opponent": "Frenzy",
                       "type": "Pool Play",
-                      "score_us": 7,
-                      "score_them": 10,
+                      "score_us": 11,
+                      "score_them": 2,
                   },
                   {
                       "gamenum": 3,
-                      "opponent": "Nemesis",
+                      "opponent": "Heist Y",
                       "type": "Pool Play",
-                      "score_us": 7,
-                      "score_them": 10,
+                      "score_us": 10,
+                      "score_them": 3,
                   },
                   {
                       "gamenum": 4,
-                      "opponent": "Spicy Tuna",
+                      "opponent": "Winnipeg Mint",
                       "type": "Pool Play",
                       "score_us": 11,
                       "score_them": 5,
                   },
                   {
                       "gamenum": 5,
-                      "opponent": "Heist X",
-                      "type": "Pool Play",
-                      "score_us": 8,
-                      "score_them": 9,
-                  },
-                  {
-                      "gamenum": 6,
                       "opponent": "Dish",
                       "type": "Pool Play",
                       "score_us": 11,
-                      "score_them": 9,
+                      "score_them": 3,
                   },
+                  {
+                      "gamenum": 6,
+                      "opponent": "MystiKuE",
+                      "type": "Quarters",
+                      "score_us": 11,
+                      "score_them": 1,
+                  },
+                  {
+                      "gamenum": 7,
+                      "opponent": "Heist X",
+                      "type": "Semis",
+                      "score_us": 8,
+                      "score_them": 11,
+                  },
+                  {
+                      "gamenum": 8,
+                      "opponent": "Heist Y",
+                      "type": "Consolation",
+                      "score_us": 0,
+                      "score_them": 0,
+                  }
               ]
           },
           {
               "tourneyid": 2,
               "sanctioned": true,
               "name": "Elite Select Challenge",
-              "teampic": "img/ESC2015_TeamPhoto.JPG",
-              "date": "July 11-12, 2015",
-              "location": "Hilliard, OH",
-              "result": "Thirteenth Place",
-              "infolink": "http://play.usaultimate.org/events/TCT-Elite-Select-Challenge-2015/",
-              "mapslink": "https://goo.gl/maps/66xTaQsLBUP2",
+              "teampic": "img/RCircleLogo_Small.png",
+              "date": "June 4-5, 2016",
+              "location": "Joliet, IL",
+              "result": "Tenth Place",
+              "infolink": "http://play.usaultimate.org/events/TCT-Elite-Select-Challenge-2016-Oshadega-Invite/schedule/Women/Club-Women/",
+              "mapslink": "https://goo.gl/maps/uXynqaMwFqF2",
               "games": [
                   {
                       "gamenum": 1,
-                      "opponent": "Nemesis",
+                      "opponent": "Green Means Go",
                       "type": "Pool Play",
-                      "score_us": 7,
-                      "score_them": 11,
-                  },
-                  {
-                      "gamenum": 2,
-                      "opponent": "Hot Metal",
-                      "type": "Pool Play",
-                      "score_us": 7,
-                      "score_them": 11,
-                  },
-                  {
-                      "gamenum": 3,
-                      "opponent": "Underground",
-                      "type": "Pool Play",
-                      "score_us": 9,
-                      "score_them": 13,
-                  },
-                  {
-                      "gamenum": 4,
-                      "opponent": "Dish",
-                      "type": "Consolation",
                       "score_us": 11,
-                      "score_them": 10,
-                  },
-                  {
-                      "gamenum": 5,
-                      "opponent": "Maeve",
-                      "type": "Consolation",
-                      "score_us": 9,
                       "score_them": 6,
                   },
                   {
-                      "gamenum": 6,
-                      "opponent": "Encore",
+                      "gamenum": 2,
+                      "opponent": "Small Batch",
+                      "type": "Pool Play",
+                      "score_us": 12,
+                      "score_them": 6,
+                  },
+                  {
+                      "gamenum": 3,
+                      "opponent": "Ozone",
+                      "type": "Pool Play",
+                      "score_us": 6,
+                      "score_them": 11,
+                  },
+                  {
+                      "gamenum": 4,
+                      "opponent": "Heist",
+                      "type": "Pre-Quarters",
+                      "score_us": 6,
+                      "score_them": 13,
+                  },
+                  {
+                      "gamenum": 5,
+                      "opponent": "Nemesis",
                       "type": "Consolation",
-                      "score_us": 11,
-                      "score_them": 10,
-                  }
+                      "score_us": 13,
+                      "score_them": 8,
+                  },
+                  {
+                      "gamenum": 6,
+                      "opponent": "Hot Metal",
+                      "type": "Consolation",
+                      "score_us": 9,
+                      "score_them": 13,
+                  },
+
               ]
-          }
+          },
+          {
+              "tourneyid": 3,
+              "sanctioned": true,
+              "name": "Chicago Heavyweights",
+              "teampic": "img/Heavyweights2016_TeamPhoto.JPG",
+              "date": "July 23-24, 2016",
+              "location": "Loves Park, IL",
+              "result": "Second Place",
+              "infolink": "http://play.usaultimate.org/events/Heavyweights-2016/schedule/Women/Club-Women/",
+              "mapslink": "https://goo.gl/maps/fXpLRVUijSw",
+              "games": [
+                  {
+                      "gamenum": 1,
+                      "opponent": "Jackwagon",
+                      "type": "Pool Play",
+                      "score_us": 15,
+                      "score_them": 3,
+                  },
+                  {
+                      "gamenum": 2,
+                      "opponent": "Dish",
+                      "type": "Pool Play",
+                      "score_us": 15,
+                      "score_them": 5,
+                  },
+                  {
+                      "gamenum": 3,
+                      "opponent": "Siege",
+                      "type": "Pool Play",
+                      "score_us": 14,
+                      "score_them": 11,
+                  },
+                  {
+                      "gamenum": 4,
+                      "opponent": "PPF",
+                      "type": "Quarters",
+                      "score_us": 12,
+                      "score_them": 6,
+                  },
+                  {
+                      "gamenum": 5,
+                      "opponent": "Notorious CLE",
+                      "type": "Semis",
+                      "score_us": 11,
+                      "score_them": 4,
+                  },
+                  {
+                      "gamenum": 6,
+                      "opponent": "Finals",
+                      "type": "Hesit",
+                      "score_us": 11,
+                      "score_them": 13,
+                  },
+
+              ]
+          },
+
     ];
 
       $scope.tournaments2015 = [
