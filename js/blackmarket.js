4f95b5e8a0301c3d3fe59427aa8583cd0a0d138a
skultimate5 <sck42@case.edu>
2016-08-16 00:37:01 -0400

    Added all that I'm currently aware of

--- js/blackmarket.js
+++ js/blackmarket.js
@@ -82,37 +82,109 @@ app.controller('MainCtrl', function($scope) {
         "id": 500,
         "title": "No Captains, No Rules",
         "image": "badge_norules.jpg",
-        "description": "Ignore a captains instructions",
+        "description": "Ignore a captains instructions"
       },
       {
         "id": 501,
         "title": "Tot's Time",
         "image": "badge_late.png",
-        "description": "Secretly make Tots late",
+        "description": "Secretly make Tots late"
       },
       {
         "id": 502,
         "title": "Car Swiper",
         "image": "badge_thief.png",
-        "description": "Steal a car",
+        "description": "Steal a car"
       },
       {
         "id": 503,
         "title": "Car Show",
         "image": "badge_wheel.jpg",
-        "description": "Visit all team cars at a tournament or practice",
+        "description": "Visit all team cars at a tournament or practice"
       },
       {
         "id": 504,
         "title": "Pants Party",
         "image": "badge_pants.png",
-        "description": "Pants Stevie or Tots",
+        "description": "Pants Stevie or Tots"
       },
       {
         "id": 505,
         "title": "Car Topper",
         "image": "badge_cartopper.jpg",
-        "description": "Sit on top of all team cars at a tournament or practice",
+        "description": "Sit on top of all team cars at a tournament or practice"
+      },
+      {
+        "id": 506,
+        "title": "One More Huck",
+        "image": "badge_huck.jpg",
+        "description": "Throw a successful huck as a good decision"
+      },
+      {
+        "id": 507,
+        "title": "Theiss-A-Like",
+        "image": "badge_theiss.jpg",
+        "description": "Stand like Theiss"
+      },
+      {
+        "id": 508,
+        "title": "Duntz",
+        "image": "badge_duntz.jpg",
+        "description": "Make a teammate look silly"
+      },
+      {
+        "id": 509,
+        "title": "Chubby Bunny",
+        "image": "badge_bunny.jpeg",
+        "description": "Post a picture with as much food in your mouth as possible"
+      },
+      {
+        "id": 510,
+        "title": "Tears of Glory",
+        "image": "badge_tears.png",
+        "description": "Make a teammate cry"
+      },
+      {
+        "id": 511,
+        "title": "#NotMyPinny",
+        "image": "badge_pinny.jpg",
+        "description": "Steal a teammate's article of clothing, and take a selfie with her, then let her find out through the black market"
+      },
+      {
+        "id": 512,
+        "title": "OB 1 Kenobi",
+        "image": "badge_ob.jpg",
+        "description": "Pull the disc far out of bounds"
+      },
+      {
+        "id": 513,
+        "title": "Cheer Commandeer",
+        "image": "badge_cheer.jpg",
+        "description": "Usurping the cheer"
+      },
+      {
+        "id": 514,
+        "title": "Sabotage",
+        "image": "badge_sabotage.jpg",
+        "description": "Self explanatory. Given for unexplanatory reasons"
+      },
+      {
+        "id": 515,
+        "title": "Spot Sniper",
+        "image": "badge_sniper.jpg",
+        "description": "Stealing someones point when not called on"
+      },
+      {
+        "id": 516,
+        "title": "Sleep Stacking",
+        "image": "badge_sleep.jpg",
+        "description": "Stacking as many objects as possible on sleeping teammates"
+      },
+      {
+        "id": 517,
+        "title": "Negligent Mother",
+        "image": "badge_badmom.jpg",
+        "description": "Leave child in car with no cracked windows"
       }
   ];
 
@@ -122,7 +194,31 @@ app.controller('MainCtrl', function($scope) {
           "name": "sadie",
           "image": "user_sadie.jpg",
           "badges": [
-            {id: 502}, {id: 504}
+            {
+              "id": 502,
+              "date_earned": "1471099029000",
+              "details": "Sophie's car"
+            }, 
+            {
+              "id": 504,
+              "date_earned": "1471108200000",
+              "details": "Pantsed Stevie"
+            },
+            {
+              "id": 506,
+              "date_earned": "1471183800000",
+              "details": "SFI Day 2"
+            },
+            {
+              "id": 507,
+              "date_earned": "1471108200000",
+              "details": "Post-lightning delay SFI"
+            },
+            {
+              "id": 512,
+              "date_earned": "1471183800000",
+              "details": "Bad"
+            }
           ]
       },
       {
@@ -130,9 +226,13 @@ app.controller('MainCtrl', function($scope) {
           "name": "ashley",
           "image": "user_ashley.jpg",
           "badges": [
+            {
+              "id": 507,
+              "date_earned": "1471108200000",
+              "details": "Post-lightning delay SFI"
+            }
 
-
-        ]
+          ]
       },
       {
           "id":3,
@@ -157,7 +257,16 @@ app.controller('MainCtrl', function($scope) {
           "name": "stevie",
           "image": "user_stevie.jpg",
           "badges": [
-            {id: 500}
+            {
+              "id": 500,
+              "date_earned": "1471120629000",
+              "details": "Went back to hotels when Tots said to stay"
+            },
+            {
+              "id": 507,
+              "date_earned": "1471108200000",
+              "details": "Post-lightning delay SFI"
+            }
           ]
       },
       {
@@ -190,7 +299,11 @@ app.controller('MainCtrl', function($scope) {
           "name": "kristen",
           "image": "user_kristen.jpg",
           "badges": [
-
+            {
+              "id": 508,
+              "date_earned": "1471183800000",
+              "details": "Sending shark attack video"
+            }
 
           ]
       },
@@ -199,7 +312,46 @@ app.controller('MainCtrl', function($scope) {
           "name": "t-lo",
           "image": "user_tlo.jpg",
           "badges": [
-            {id: 502}, {id: 503}, {id: 505}
+            {
+              "id": 502,
+              "date_earned": "1471099029000",
+              "details": "Sophie's car"
+            }, 
+            {
+              "id": 503,
+              "date_earned": "1471099029000",
+              "details": "SFI Lightning Delay"
+            }, 
+            {
+              "id": 505,
+              "date_earned": "1471099029000",
+              "details": "SFI Lightning Delay"
+            },
+            {
+              "id": 507,
+              "date_earned": "1471108200000",
+              "details": "Post-lightning delay SFI"
+            },
+            {
+              "id": 512,
+              "date_earned": "1471183800000",
+              "details": "Bad"
+            },
+            {
+              "id": 513,
+              "date_earned": "1471183800000",
+              "details": "Silly T-lo"
+            },
+            {
+              "id": 516,
+              "date_earned": "1471183800000",
+              "details": "Not even one object on basically awake Brittany"
+            },
+            {
+              "id": 517,
+              "date_earned": "1471183800000",
+              "details": "Left Brittany in car"
+            }
           ]
       },
       {
@@ -207,7 +359,26 @@ app.controller('MainCtrl', function($scope) {
           "name": "zeus",
           "image": "user_zeus.jpg",
           "badges": [
-            {id: 500}, {id: 502}
+            {
+              "id": 500,
+              "date_earned": "1471120629000",
+              "details": "Went back to hotels when Tots said to stay"
+            }, 
+            {
+              "id": 502,
+              "date_earned": "1471099029000",
+              "details": "Sophie's car"
+            },
+            {
+              "id": 507,
+              "date_earned": "1471108200000",
+              "details": "Post-lightning delay SFI"
+            },
+            {
+              "id": 516,
+              "date_earned": "1471183800000",
+              "details": "Many objects on sleepy Stevie"
+            }
           ]
       },
       {
@@ -215,7 +386,11 @@ app.controller('MainCtrl', function($scope) {
           "name": "tz",
           "image": "user_tz.jpg",
           "badges": [
-
+            {
+              "id": 507,
+              "date_earned": "1471108200000",
+              "details": "Post-lightning delay SFI"
+            }
           ]
       },
       {
@@ -223,6 +398,11 @@ app.controller('MainCtrl', function($scope) {
           "name": "brittany",
           "image": "user_brittany.jpg",
           "badges": [
+            {
+              "id": 507,
+              "date_earned": "1471108200000",
+              "details": "Post-lightning delay SFI"
+            }
 
           ]
       },
@@ -231,7 +411,11 @@ app.controller('MainCtrl', function($scope) {
           "name": "jules",
           "image": "user_jules.jpg",
           "badges": [
-
+            {
+              "id": 508,
+              "date_earned": "1471183800000",
+              "details": "Making fun of Kristen's Pitts?"
+            }
           ]
       },
       {
@@ -247,7 +431,26 @@ app.controller('MainCtrl', function($scope) {
           "name": "sophie",
           "image": "user_sophie.jpg",
           "badges": [
-            {id: 500}, {id: 503}, {id: 505}
+            {
+              "id": 500,
+              "date_earned": "1471120629000",
+              "details": "Went back to hotels when Tots said to stay"
+            }, 
+            {
+              "id": 503,
+              "date_earned": "1471099029000",
+              "details": "SFI Lightning Delay"
+            },
+            {
+              "id": 505,
+              "date_earned": "1471099029000",
+              "details": "SFI Lightning Delay"
+            },
+            {
+              "id": 507,
+              "date_earned": "1471108200000",
+              "details": "Post-lightning delay SFI"
+            }
           ]
       },
       {
@@ -255,7 +458,11 @@ app.controller('MainCtrl', function($scope) {
           "name": "dani",
           "image": "user_dani.jpg",
           "badges": [
-
+            {
+              "id": 507,
+              "date_earned": "1471108200000",
+              "details": "Post-lightning delay SFI"
+            }
           ]
       },
       {
@@ -263,35 +470,64 @@ app.controller('MainCtrl', function($scope) {
           "name": "talia",
           "image": "user_talia.jpg",
           "badges": [
-
+            { 
+              "id": 507,
+              "date_earned": "1471108200000",
+              "details": "Post-lightning delay SFI"
+            }
           ]
       },
       {
           "id":27,
           "name": "bailey",
           "image": "user_bailey.jpg",
-          "badges": []
+          "badges": [
+            {
+              "id": 507,
+              "date_earned": "1471108200000",
+              "details": "Post-lightning delay SFI"
+            }
+          ]
       },
       {
           "id":28,
           "name": "monty",
           "image": "user_monty.jpg",
           "badges": [
-
+            {
+              "id": 507,
+              "date_earned": "1471108200000",
+              "details": "Post-lightning delay SFI"
+            }
           ]
       },
       {
           "id":30,
           "name": "jodi",
           "image": "user_jodi.jpg",
-          "badges": []
+          "badges": [
+          {
+              "id": 507,
+              "date_earned": "1471108200000",
+              "details": "Post-lightning delay SFI"
+            }
+            ]
       },
       {
           "id":31,
           "name": "sara scott",
           "image": "user_sarascott.jpg",
           "badges": [
-
+            {
+              "id": 514,
+              "date_earned": "1471183800000",
+              "details": "Reasons have not been given. SS sabotage needs no reasoning"
+            },
+            {
+              "id": 515,
+              "date_earned": "1471183800000",
+              "details": "Always wants to play"
+            }
           ]
       },
       {
@@ -299,7 +535,16 @@ app.controller('MainCtrl', function($scope) {
           "name": "hank",
           "image": "user_hank.jpg",
           "badges": [
-
+            {
+              "id": 507,
+              "date_earned": "1471108200000",
+              "details": "Post-lightning delay SFI"
+            },
+            {
+              "id": 515,
+              "date_earned": "1471183800000",
+              "details": "Always wants to play"
+            }
           ]
       },
       {
