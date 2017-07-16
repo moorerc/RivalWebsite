f2269d40a31406bb1e70f70d3e91d3576c1780eb
Becky Moore <bmoore@palantir.com>
2017-07-16 14:37:22 -0700

    initial commit to move website to react app

--- configs/webpack.config.js
+++ configs/webpack.config.js
@@ -1,184 +1,79 @@
-const path = require('path');
-const webpack = require('webpack');
-const DashboardPlugin = require('webpack-dashboard/plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CheckerPlugin } = require("awesome-typescript-loader");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const webpack = require("webpack");
 
-const PATHS = {
-  root: path.resolve(__dirname, '..'),
-  nodeModules: path.resolve(__dirname, '../node_modules'),
-  src: path.resolve(__dirname, '../src'),
-  dist: path.resolve(__dirname, '../dist'),
-};
+const IS_PRODUCTION = process.env.NODE_ENV === "production";
 
-const DEV_SERVER = {
-  hot: true,
-  hotOnly: true,
-  historyApiFallback: true,
-  overlay: true,
-  // stats: 'verbose',
-  // proxy: {
-  //   '/api': 'http://localhost:3000'
-  // },
-};
+const plugins = [
+    new CheckerPlugin(),
+    new ExtractTextPlugin("[name].css"),
+];
+if (IS_PRODUCTION) {
+    plugins.push(
+        new webpack.optimize.UglifyJsPlugin(),
+        new webpack.DefinePlugin({
+            "process.env": {
+                NODE_ENV: JSON.stringify("production"),
+            },
+        })
+    );
+} else {
+    plugins.push(
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+        })
+    );
+}
 
-module.exports = (env = {}) => {
-  console.log({ env });
-  const isBuild = !!env.build;
-  const isDev = !env.build;
-  const isSourceMap = !!env.sourceMap || isDev;
-
-  return {
-    cache: true,
-    devtool: isDev ? 'eval-source-map' : 'source-map',
-    devServer: DEV_SERVER,
-
-    context: PATHS.root,
-
+module.exports = {
     entry: {
-      app: [
-        'react-hot-loader/patch',
-        './src/index.tsx',
-      ],
+        app: [
+            "./src/index.tsx",
+            "./src/styles/scss/index.scss",
+        ],
     },
+
     output: {
-      path: PATHS.dist,
-      filename: isDev ? '[name].js' : '[name].[hash].js',
-      publicPath: '/',
-      // chunkFilename: '[id].chunk.js',
+        filename: "[name].js",
+        path: path.resolve(__dirname, "../dist"),
     },
 
     resolve: {
-      extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
-      modules: ['src', 'node_modules'],
+        extensions: [ ".js", ".jsx", ".ts", ".tsx", ".scss" ],
     },
 
-    // externals: {
-    // },
+    devtool: IS_PRODUCTION ? false : "cheap-module-eval-source-map",
 
     module: {
-      rules: [
-        // typescript
+      loaders: [
         {
-          test: /\.tsx?$/,
-          include: PATHS.src,
-          use: (env.awesome ?
-            [
-              { loader: 'react-hot-loader/webpack' },
-              {
-                loader: 'awesome-typescript-loader',
-                options: {
-                  transpileOnly: true,
-                  useTranspileModule: false,
-                  sourceMap: isSourceMap,
-                },
-              },
-            ] : [
-              { loader: 'react-hot-loader/webpack' },
-              {
-                loader: 'ts-loader',
-                options: {
-                  transpileOnly: true,
-                  compilerOptions: {
-                    'sourceMap': isSourceMap,
-                    'target': isDev ? 'es2015' : 'es5',
-                    'isolatedModules': true,
-                    'noEmitOnError': false,
-                  },
-                },
-              },
-            ]
-          ),
+            test: /\.tsx?$/,
+            loader: "awesome-typescript-loader",
+            options: {
+                configFileName: "./src/tsconfig.json",
+            },
         },
-        // json
         {
-          test: /\.json$/,
-          include: [PATHS.src],
-          use: { loader: 'json-loader' },
+            test: /\.scss$/,
+            loader: ExtractTextPlugin.extract({
+                fallback: "style-loader",
+                use: "css-loader!sass-loader",
+            }),
         },
-        // // css
-        // {
-        //   test: /\.css$/,
-        //   include: [PATHS.STYLES],
-        //   loader: ExtractTextPlugin.extract([
-        //     'css-loader?{modules: false}',
-        //     'postcss-loader',
-        //   ]),
-        // },
-        // // less
-        // {
-        //   test: /\.less$/,
-        //   include: [PATHS.STYLES],
-        //   loader: ExtractTextPlugin.extract([
-        //     'css-loader?{modules: false}',
-        //     'less-loader',
-        //   ]),
-        // },
-        // // images
-        // {
-        //   test: /\.(jpg|jpeg|png|gif|svg)$/,
-        //   include: [PATHS.IMAGES],
-        //   use: {
-        //     loader: 'url-loader',
-        //     options: {
-        //       name: 'images/[hash].[ext]',
-        //       limit: 1000, // inline file data until size
-        //     },
-        //   },
-        // },
-        // // fonts
-        // {
-        //   test: /\.(woff|woff2|ttf|eot)$/,
-        //   include: [
-        //     PATHS.ASSETS,
-        //   ],
-        //   use: {
-        //     loader: 'file-loader',
-        //     options: {
-        //       name: 'fonts/[name].[hash].[ext]',
-        //     },
-        //   },
-        // },
       ],
     },
 
-    plugins: [
-      new DashboardPlugin(),
-      new webpack.DefinePlugin({
-        'process.env': {
-          NODE_ENV: JSON.stringify(isDev ? 'development' : 'production'),
-        },
-      }),
-      new webpack.optimize.CommonsChunkPlugin({
-        name: 'vendor',
-        minChunks: (module) => module.context && module.context.indexOf('node_modules') !== -1,
-      }),
-      new webpack.optimize.CommonsChunkPlugin({
-        name: 'manifest',
-      }),
-      ...(isDev ? [
-        new webpack.HotModuleReplacementPlugin({
-          // multiStep: true, // better performance with many files
-        }),
-        new webpack.NamedModulesPlugin(),
-      ] : []),
-      ...(isBuild ? [
-        new webpack.LoaderOptionsPlugin({
-          minimize: true,
-          debug: false
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-          beautify: false,
-          compress: {
-            screw_ie8: true
-          },
-          comments: false,
-          sourceMap: isSourceMap,
-        }),
-        new HtmlWebpackPlugin({
-          template: './index.html',
-        }),
-      ] : []),
-    ]
-  };
+    devServer: {
+      headers: {
+          "Access-Control-Allow-Origin": "*",
+      },
+      historyApiFallback: true,
+      //https: false,
+      inline: true,
+      stats: "errors-only",
+    },
 
+    plugins,
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,46 +1,12 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-import { App } from './app';
+import { Root } from './Root';
 
 const renderRoot = (app: JSX.Element) => {
-  ReactDOM.render(app, document.getElementById('root'));
+  ReactDOM.render(app, document.getElementById('app'));
 };
 
-if (process.env.NODE_ENV === 'production') {
-  renderRoot((
-    <App />
+renderRoot((
+    <Root />
   ));
-} else { // removed in production, hot-reload config
-  const HotContainer = require('react-hot-loader').AppContainer;
-  renderRoot((
-    <HotContainer>
-      <App />
-    </HotContainer>
-  ));
-
-  if (module.hot) {
-    // app
-    module.hot.accept('./app', async () => {
-      // const NextApp = require('./app').App;
-      const NextApp = (await System.import('./app')).App;
-      renderRoot((
-        <HotContainer>
-          <NextApp />
-        </HotContainer>
-      ));
-    });
-
-    // // reducers
-    // module.hot.accept('../modules/root-reducer', () => {
-    //   const newRootReducer = require('./root-reducer').default;
-    //   store.replaceReducer(newRootReducer);
-    // });
-
-    // // epics
-    // module.hot.accept('../modules/root-epic', () => {
-    //   const newRootEpic = require('./root-epic').default;
-    //   epicMiddleware.replaceEpic(newRootEpic);
-    // });
-  }
-}
